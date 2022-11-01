210d3b7617bd07e3da379d8803f33cecb207b35d
Mariia Korol <marie.korol1991@gmail.com>
2022-11-01 16:31:12 +0200

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const htmlmin = require('gulp-htmlmin');
@@ -81,9 +80,9 @@ exports.clean = cleanTask;
 exports.include = htmlIncludeTask;
 exports.default = gulp.series(
   cleanTask,
-  htmlIncludeTask,
   sassTask,
   imageTask,
   htmlTask,
+  htmlIncludeTask,
   // watchTask,
 );
